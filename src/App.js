c9a1a1cae1299fa8b143a7bad4186e28a73d657a
jaseelamahar <jaseelamahar@outlook.com>
2025-01-11 18:23:25 +0530

    added all

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ function App() {
   const [bookmarks, setBookmarks] = useState([]);
   const [editingIndex, setEditingIndex] = useState(null);
 
-  // handleAdd function for both adding and editing
+  
   const handleAdd = (title, url) => {
     if (editingIndex !== null) {
-      // If editing, update the existing bookmark
+  
       const updatedBookmarks = bookmarks.map((bookmark, i) =>
         i === editingIndex ? { title, url } : bookmark
       );
       setBookmarks(updatedBookmarks);
     } else {
-      // Otherwise, add a new bookmark
+      
       setBookmarks([...bookmarks, { title, url }]);
     }
-    setIsModalopen(false); // Close modal after action
-    setEditingIndex(null);  // Reset editing index
+    setIsModalopen(false); 
+    setEditingIndex(null); 
   };
 
   const handleEdit = (index) => {
